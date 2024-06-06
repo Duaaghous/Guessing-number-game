5f4286c8d2347dadff9d7c2e336e6ba8f5fcb451
duaaghous <105564566+Duaaghous@users.noreply.github.com>
2024-06-07 06:24:36 +1400

    you can use this code

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 #! /usr/bin/env node
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = __importDefault(require("inquirer"));
+import inquirer from "inquirer";
 // 1.computer will generate a random number
 // 2.user input for guessing number
 // 3.compare user input with computer generated number and show result
 const randomNum = Math.floor(Math.random() * 6 + 1);
 console.log(randomNum);
 console.log("START");
-const answer = await inquirer_1.default.prompt([
+const answer = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
